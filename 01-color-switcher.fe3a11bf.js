dba3d4b63e2323057c75ac1233680c3a40e299e0
Ruslan-Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2023-08-21 23:05:51 +0000

    Deploying to gh-pages from @ Ruslan-Tretiak/goit-js-hw-09@938f939b89edffaa1817fcdc5bc70aefd533c292 🚀

--- 01-color-switcher.fe3a11bf.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body,n=null;function d(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}t.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,n=setInterval(d,1e3)})),e.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,clearInterval(n)}))}();
-//# sourceMappingURL=01-color-switcher.fe3a11bf.js.map
