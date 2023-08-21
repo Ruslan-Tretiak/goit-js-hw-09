ed8c24eaeed338805ccbcf04078a08512fabf072
Ruslan Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2023-08-21 22:28:21 +0100

    Task #1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+let changeInterval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+function changeColorRandom() {
+    body.style.backgroundColor = getRandomHexColor();
+} 
+
+startButton.addEventListener('click', () => {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    changeInterval = setInterval(changeColorRandom, 1000);
+});
+stopButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    clearInterval(changeInterval);
+}); 
