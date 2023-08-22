47b5eeae59100c60849614a22883dbbf05dfd7a2
Ruslan Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2023-08-22 22:19:02 +0100

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const firstDelay = Number(event.target.delay.value);
+  const delayStep = Number(event.target.step.value);
+  const amount = Number(event.target.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const currentDelay = firstDelay + i * delayStep;
+
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
